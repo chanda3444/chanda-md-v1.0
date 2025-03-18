9c0fadb2992f921bb25427704ca26cb9f64ef8f8
chanda3444 <doctorchanda805@gmail.com>
2025-03-18 11:07:13 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,25 @@
+{
+  "name": "chanda-md",
+  "version": "1.0.0",
+  "private": true,
+  "type": "module",
+  "scripts": {
+    "start": "node bot.js",
+    "dev": "node bot.js"
+  },
+  "dependencies": {
+    "@adiwajshing/keyed-db": "0.2.4",
+    "@hapi/boom": "10.0.1",
+    "@whiskeysockets/baileys": "6.5.0",
+    "dotenv": "16.3.1",
+    "qrcode-terminal": "0.12.0",
+    "pino": "8.16.2",
+    "pino-pretty": "10.2.3",
+    "jimp": "0.22.10",
+    "link-preview-js": "3.0.5",
+    "sharp": "0.32.6",
+    "yt-search": "^2.10.4",
+    "ytdl-core": "^4.11.5",
+    "fluent-ffmpeg": "^2.1.2"
+  }
+}
